eb43dd949f13f48f6aab1ecc4362c7f95ccb7ae7
mihailVishnyakov <alunos13@yandex.ru>
2020-06-21 17:17:50 +0300

    Update click.js

--- jsBrHomework/websocket/bubbles/js/click.js
+++ jsBrHomework/websocket/bubbles/js/click.js
@@ -1,6 +1,19 @@
 'use strict';
 
 let bubleConnect = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
 bubleConnect.addEventListener('open', () => {
-	console.log(showBubbles(bubleConnect));
+	console.log('Вебсокет-соединение открыто');
+});
+
+bubleConnect.send(JSON.stringify({
+	test: 'ТЕСТ'
+}));
+
+bubleConnect.addEventListener('message', event => {
+	console.log(`сообщение с сервера: ${event.data}`);
+});
+
+bubleConnect.addEventListener('error', error => {
+	console.log(`ошибка: ${error.data}`);
 });
