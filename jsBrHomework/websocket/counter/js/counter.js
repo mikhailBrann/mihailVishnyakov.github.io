abebfaafb3d063fe1727826b1f2c1ee283a919c8
mihailVishnyakov <alunos13@yandex.ru>
2020-06-21 19:14:45 +0300

    counter final

--- jsBrHomework/websocket/counter/js/counter.js
+++ jsBrHomework/websocket/counter/js/counter.js
@@ -1,1 +1,23 @@
 'use strict';
+let counterConnect;
+
+window.addEventListener('load', () => {
+	counterConnect = new WebSocket('wss://neto-api.herokuapp.com/counter');
+	let counterWrapp = document.getElementsByClassName('counter')[0];
+	let errorsWrapp = document.querySelector('output.errors');
+
+	counterConnect.addEventListener('open', () => {
+		counterConnect.addEventListener('message', event => {
+			let counterData = JSON.parse(event.data);
+
+			counterWrapp.innerText = counterData.connections;
+			errorsWrapp.innerText = counterData.errors;
+			console.log(counterData);
+		});
+	});
+});
+
+
+window.addEventListener('beforeunload', () => {
+	counterConnect.close(1000);
+});
