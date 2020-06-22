064d651005f30b01ce254a631189d695feb8b1ec
mihailVishnyakov <alunos13@yandex.ru>
2020-06-22 23:27:32 +0300

    chat final

--- jsBrHomework/websocket/chat/js/chat.js
+++ jsBrHomework/websocket/chat/js/chat.js
@@ -10,76 +10,119 @@ let chatStatus = chatWrapp.getElementsByClassName('chat-status')[0];
 let messagesContent = chatWrapp.getElementsByClassName('messages-content')[0];
 let messagesTemplates = chatWrapp.getElementsByClassName('messages-templates')[0];
 
+//создаем дату для сообщений
+let thisDate = () => {
+	let dateMessage = new Date();
+	let dateResult = `${dateMessage.getHours() <= 9 ? '0' : ''}${dateMessage.getHours()}:${dateMessage.getMinutes() <= 9 ? '0' : ''}${dateMessage.getMinutes()}`
+	return dateResult;
+};
+
+//удаляем лишние сообщения из чата
+let removeMess = () => {
+	let messagesContentLenght = chatWrapp.getElementsByClassName('messages-content')[0].children.length;
+	if(messagesContentLenght >= 3) {
+ 		document.querySelector('.messages-content *').remove(); 
+	}
+};
 
 
-
-
-
-
+//обработка открытия WEbSocket
 window.addEventListener('load', () => {
 	let chatConnected = new WebSocket('wss://neto-api.herokuapp.com/chat');
+
+	//вешаем события по клику и enter
+	messageSubmit.addEventListener('click', sendingMessage);
+	messageSubmit.addEventListener('keyup', (event) => {
+		if(event.enter) {
+			sendingMessage();
+		}
+	});
+
+	//функция отправки сообщений
+	function sendingMessage(event) {
+		event.preventDefault();
+
+		let userMessage = {
+			userMess: messageInput.value,
+			userDate: thisDate()
+		};
+
+		//отсылаем сообщение пользователя
+		chatConnected.send(JSON.stringify(userMessage));
+
+		//выводим сообщение пользователя
+		let userPublickMess = messagesTemplates.querySelector('.message-personal').cloneNode(true);
+		userPublickMess.querySelector('.message-text').innerText = userMessage.userMess;
+		userPublickMess.querySelector('.timestamp').innerText = userMessage.userDate;
+		messagesContent.appendChild(userPublickMess);
+
+		//возвращаем состояние поля ввода после отправки формы
+		messageInput.value = '';
+		//усли много сообщений в чате, то удаляем первое
+		removeMess();
+	}
 
 	chatConnected.addEventListener('open', () => {
 		chatStatus.innerText = chatStatus.getAttribute('data-online');
 		messageSubmit.removeAttribute('disabled');
 
-		chatConnected.addEventListener('close', ()=> {
-			chatStatus.innerText = chatStatus.getAttribute('data-offline');
-			messageSubmit.addAttribute('disabled');
-		});
+		//установка статуса пользователя
+		let userStatus = messagesTemplates.querySelector('.message-status').cloneNode(true);
+		userStatus.querySelector('.message-text').innerText = '«Пользователь появился в сети»';
+		messagesContent.appendChild(userStatus);
+		
+
 	});
+
+	//обработка закрытия WEbSocket
+	chatConnected.addEventListener('close', () => {
+
+		chatStatus.innerText = chatStatus.getAttribute('data-offline');
+		messageSubmit.setAttribute('disabled', '');
+
+		let userStatus = messagesTemplates.querySelector('.message-status').cloneNode(true);
+		userStatus.querySelector('.message-text').innerText = '«Пользователь не в сети»';
+		messagesContent.appendChild(userStatus);
+	});
+
 
 	chatConnected.addEventListener('message', (event) => {
-		console.log(event.data);
+	
+		//обрабатываем поведение бота
+		let botStatus = messagesTemplates.querySelector('.loading').cloneNode(true);
+		
+		if(event.data == '...'){
+			botStatus.querySelector('span').innerText = 'собеседник сейчас печатает сообщение';
+			messagesContent.appendChild(botStatus);
+		} else {
+			//удаляем уведомление о том, что бот печатает
+			if(messagesContent.querySelector('.loading')){
+				botStatus.remove();
+			}
+
+			//выводим сообщение бота
+			let botMessage = {
+				botMess: event.data,
+				botDate: thisDate()
+			};
+
+			//ищем шаблон сообщений для бота
+			let seachBotMessElem = messagesTemplates.querySelectorAll('.message');
+
+			seachBotMessElem.forEach((botMess) => {
+				if(botMess.className == 'message') {
+					let botPublickMess = botMess.cloneNode(true);
+
+					botPublickMess.querySelector('.message-text').innerText = botMessage.botMess;
+					botPublickMess.querySelector('.timestamp').innerText = botMessage.botDate;
+
+					messagesContent.appendChild(botPublickMess);
+				}
+			});
+		}
+		
 	});
 
-	setTimeout(1000, () => chatConnected.close(1000));
 });
 
 
-/*
-Интерфейс
-Весь интерфейс чата сосредоточен в теге с классом chat. Выполняйте поиск компонентов чата только в нём.+
-
-Форма отправки нового сообщения имеет класс message-box. Поле ввода сообщения имеет класс message-input.+ 
-Кнопка «Отправить сообщение» имеет класс message-submit.+
-
-Все новые сообщения для отображения необходимо отображать в блоке с классом messages-content.+
-
-
-Есть четыре типа сообщений. Их шаблоны доступны в скрытом контейнере с классом messages-templates, у всех имеется класс message:
-
-
-Сообщение с классом loading — информация о том, что собеседник сейчас печатает сообщение.
-
-Сообщение от собеседника без дополнительного класса и сообщение от пользователя с классом message-personal имеют элементы с классами message-text и timestamp,
-в тела которых следует поместить текст сообщения и время добавления.
-
-Сообщение с классом message-status — вывод разных уведомлений. Также имеет элемент с классами message-text для вывода текста уведомления.
-
-Элемент с классом chat-status. Необходимо использовать для вывода состояния чата. 
-
-Если собеседник в сети, то следует подставить в тело текст из атрибута data-online, иначе — из data-offline.
-
-
-
-
-
-Функционал
-При открытии страницы необходимо установить соединение с веб-сокетом по адресу wss://neto-api.herokuapp.com/chat.+
-
-Если соединение успешно установлено, необходимо обновить статус чата,
-а также активировать кнопку «Отправить сообщение» и вывести уведомление с текстом «Пользователь появился в сети».+
-
-При получении сообщения по веб-сокет соединению необходимо проверить текст сообщения. Если он равен ... (три точки),
-то необходимо отобразить информацию о том, что собеседник сейчас печатает сообщение. Если текст сообщения другой,
-то необходимо отобразить сообщение с этим текстом. А информацию о том, что собеседник печатает, необходимо удалить.
-
-При отправке сообщения пользователем через форму (кнопка «Отправить сообщение» или клавиша Enter в поле ввода сообщения) необходимо 
-текст сообщения отправить по веб-сокет соединению и отобразить сообщение пользователя в общем списке.
-
-Время сообщений в обоих случаях берем текущее на момент отображения. Учитывайте, что часы и минуты всегда должны иметь 2 символа, например, 09:03.
-
-Если соединение с веб-сокет сервером закрывается, то необходимо поменять статус чата и деактивировать кнопку «Отправить сообщение»,
-а также вывести уведомление с текстом «Пользователь не в сети».
-*/
