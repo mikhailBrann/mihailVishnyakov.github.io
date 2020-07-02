450ca7920900bdb0838e2349ff196be86a20d03c
mihailVishnyakov <alunos13@yandex.ru>
2020-07-02 23:39:24 +0300

    twitter final

--- jsBrHomework/cors/twitter/js/twitter.js
+++ jsBrHomework/cors/twitter/js/twitter.js
@@ -1,1 +1,24 @@
 'use strict';
+
+Promise.all([loadTwitt('callback', 'https://neto-api.herokuapp.com/twitter/jsonp')]).then(createTwittCart);
+
+
+function loadTwitt(callbackName,dataUrl) {
+	return new Promise((done, fall) => {
+		const genericScript = document.createElement('script');
+		genericScript.src = `${dataUrl}?callback=${callbackName}`;
+		document.body.appendChild(genericScript);
+		window[callbackName] = done;
+	});
+}
+
+function createTwittCart(data) {
+	let twitterContainer = document.getElementsByClassName('container')[0];
+	twitterContainer.querySelector('[data-wallpaper]').src = data.wallpaper;
+	twitterContainer.querySelector('[data-username]').innerText = data.username;
+	twitterContainer.querySelector('[data-description]').innerText = data.description;
+	twitterContainer.querySelector('[data-pic]').src = data.pic;
+	twitterContainer.querySelector('[data-tweets]').innerText = data.tweets;
+	twitterContainer.querySelector('[data-followers]').innerText = data.followers;
+	twitterContainer.querySelector('[data-following]').innerText = data.following;
+}
