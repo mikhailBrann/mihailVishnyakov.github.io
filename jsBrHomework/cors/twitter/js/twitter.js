2b49d685775002d7fcee9359bf4cf92080a08320
mihailVishnyakov <alunos13@yandex.ru>
2020-07-03 00:01:09 +0300

    Update twitter.js

--- jsBrHomework/cors/twitter/js/twitter.js
+++ jsBrHomework/cors/twitter/js/twitter.js
@@ -3,23 +3,23 @@
 Promise.all([loadTwitt('callback', 'https://neto-api.herokuapp.com/twitter/jsonp')]).then(createTwittCart);
 
 
-function loadTwitt(callbackName, dataUrl) {
+function loadTwitt(callbackName, data[0]Url) {
 	return new Promise((done, fail) => {
 		const genericScript = document.createElement('script');
-		genericScript.src = `${dataUrl}?callback=${callbackName}`;
+		genericScript.src = `${data[0]Url}?callback=${callbackName}`;
 		document.body.appendChild(genericScript);
 		window[callbackName] = done;
 	});
 }
 
 function createTwittCart(data) {
-	console.log(data);
+	console.log(data[0]);
 	let twitterContainer = document.getElementsByClassName('container')[0];
-	twitterContainer.querySelector('[data-wallpaper]').src = data.wallpaper;
-	twitterContainer.querySelector('[data-username]').innerText = data.username;
-	twitterContainer.querySelector('[data-description]').innerText = data.description;
-	twitterContainer.querySelector('[data-pic]').src = data.pic;
-	twitterContainer.querySelector('[data-tweets]').innerText = data.tweets;
-	twitterContainer.querySelector('[data-followers]').innerText = data.followers;
-	twitterContainer.querySelector('[data-following]').innerText = data.following;
+	twitterContainer.querySelector('[data[0]-wallpaper]').src = data[0].wallpaper;
+	twitterContainer.querySelector('[data[0]-username]').innerText = data[0].username;
+	twitterContainer.querySelector('[data[0]-description]').innerText = data[0].description;
+	twitterContainer.querySelector('[data[0]-pic]').src = data[0].pic;
+	twitterContainer.querySelector('[data[0]-tweets]').innerText = data[0].tweets;
+	twitterContainer.querySelector('[data[0]-followers]').innerText = data[0].followers;
+	twitterContainer.querySelector('[data[0]-following]').innerText = data[0].following;
 }
